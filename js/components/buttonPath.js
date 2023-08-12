18ef427862e50e25c9557551b212994d8ef4a807
edmedev <edem0142@gmail.com>
2023-08-11 20:02:59 -0500

    Creando la página de login

---
+++ js/components/buttonPath.js
@@ -0,0 +1,9 @@
+function buttonPath() {
+    let buttonViewConsoles = document.getElementById("buttonViewConsoles");
+    buttonViewConsoles.addEventListener("click", function() {
+        window.location.href = "#ver-consolas";
+    })
+}
+
+export default buttonPath;
+
--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // importando los componetes
-import importTemplates from "./components/importTemplates.js"
+import importTemplates from "./components/importTemplates.js";
+import buttonPath from "./components/buttonPath.js";
 
 // Llamando las funciones
 importTemplates();
+buttonPath();
 
